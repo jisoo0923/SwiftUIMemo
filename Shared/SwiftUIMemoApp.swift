8ae2ae5af4a67257bb304db7803d790e66b90d29
Jisoo Park <scalett20041@gmail.com>
2022-05-24 16:07:41 +0900

    Step1_2_3

--- Shared/SwiftUIMemoApp.swift
+++ Shared/SwiftUIMemoApp.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 @main
 struct SwiftUIMemoApp: App {
+    @StateObject var store = MemoStore()    // memo 저장소를 속성으로 먼저 저장
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store)       // MainListView에 등록 -> MainListView에서 이어지는 View에서 동일한 객체 쉽게 사용가능
         }
     }
 }
