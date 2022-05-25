aa5eb3f05c3fb116dd904cad3e1314154ecf0989
Jisoo Park <scalett20041@gmail.com>
2022-05-25 12:37:10 +0900

    Step6: memo writing screen

---
+++ Shared/View/ComposeView.swift
@@ -0,0 +1,55 @@
+//
+//  ComposeView.swift
+//  SwiftUIMemo
+//
+//  Created by 박지수 on 2022/05/25.
+//
+
+import SwiftUI
+
+struct ComposeView: View {
+    @EnvironmentObject var store: MemoStore
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var content: String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextEditor(text: $content)
+                    .padding()
+            }
+            .navigationTitle("새 메모")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading)
+                    {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("취소")
+                    }
+                }
+                
+                ToolbarItemGroup(placement: .navigationBarTrailing)
+                    {
+                    Button {
+                        store.insert(memo: content)     // 저장기능 구현
+                        
+                        dismiss()
+                    } label: {
+                        Text("저장")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ComposeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComposeView()
+            .environmentObject(MemoStore())
+    }
+}
